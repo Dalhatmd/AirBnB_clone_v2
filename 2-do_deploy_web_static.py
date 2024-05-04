d8490aaec42e883ba84a74794263197977835c29
Dalhatmd <umarwalid4055@gmail.com>
2024-05-04 09:27:08 -0700

    Add task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" Deploys to servers """
+from fabric import task, Connection
+import os
+
+
+WEB_SERVERS = [
+        {
+            "host": "100.25.19.204",
+            "user": "ubuntu",
+            "key_filename": "~/.ssh/id_rsa",
+            },
+        {
+            "host": "54.157.159.85",
+            "user": "ubuntu",
+            "key_filename": "~/.ssh/id_rsa",
+            },
+        ]
+
+
+@task
+def do_deploy(c, archive_path):
+    """Deploys an archive to multiple servers"""
+
+    if not os.path.exists(archive_path):
+        return False
+
+    archive_filename = os.path.basename(archive_path)
+    if not archive_filename.endswith(".tgz"):
+        return False
+
+    base_name = archive_filename[:-4]
+
+    target_dir = f"/data/web_static/releases/{base_name}"
+
+    successful = True
+    for server in WEB_SERVERS:
+        try:
+            conn = Connection(
+                host=server["host"],
+                user=server["user"],
+                connect_kwargs={
+                    "key_filename": server["key_filename"],
+                },
+            )
+
+            conn.put(archive_path, "/tmp/")
+
+            conn.run(f"sudo mkdir -p {target_dir}")
+
+            conn.run(f"sudo tar -xzf /tmp/{archive_filename} -C {target_dir}")
+
+            conn.run(f"sudo rm /tmp/{archive_filename}")
+
+            conn.run(f"sudo mv {target_dir}/web_static/* {target_dir}/")
+
+            conn.run(f"sudo rm -rf {target_dir}/web_static")
+
+            conn.run(f"sudo rm -rf /data/web_static/current")
+
+            conn.run(f"sudo ln -s {target_dir} /data/web_static/current")
+
+        except Exception as e:
+            print(f"Error during deployment on {server['host']}: {e}")
+            successful = False
+
+    return successful
