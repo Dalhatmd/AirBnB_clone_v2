eb79182bda8782d6cb30955fb19a802a5beedf93
Dalhatmd <umarwalid4055@gmail.com>
2024-05-17 15:44:11 +0100

    Fix pycodestyle

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """ Module to start a Flask web application """
 from flask import Flask
-
-
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display a message """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Display a message """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ Display a message """
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -25,10 +28,12 @@ def python_text(text):
     """ Display a message """
     return 'Python %s' % text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     """ Display a message """
     return '{} is a number'.format(n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
